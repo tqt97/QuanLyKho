898adef95777156cdd19a99cb404274fbf429d54
QuocTuanIT <kutuanonline199@gmail.com>
2024-08-19 08:37:28 +0700

    install filament admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'TuanTQ',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12341234'),
         ]);
     }
 }
