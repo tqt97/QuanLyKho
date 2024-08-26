6162a189fd6d47bd58fe9d9fa982322a3a57ff23
tqt97 <kutuanonline199@gmail.com>
2024-08-26 07:14:49 +0700

    fix null order in product overview

--- app/Filament/Resources/ProductResource/Widgets/ProductsOverview.php
+++ app/Filament/Resources/ProductResource/Widgets/ProductsOverview.php
@@ -22,21 +22,21 @@ class ProductsOverview extends BaseWidget
             ->selectRaw('count(*) as total')
             ->groupBy('product_id')
             ->orderByDesc('total')
-            ->first()
-            ->product_id;
-        $popular_products = Product::find($id_popular)->first();
+            ->first();
+
+        $popular_products = $id_popular ? Product::find($id_popular['product_id'])?->first() : null;
 
         // get product with the most price
         $id_price = Product::max('sell_price');
         // dd($id_price);
-        $most_expensive_product = Product::where('sell_price', $id_price)->first();
+        $most_expensive_product = Product::where('sell_price', $id_price)->first() ?? null;
 
         return [
             Stat::make('total_products', $total_products)
                 ->label(__('shop/product.total_products')),
-            Stat::make('most_popular_product', $popular_products['common_title'])
+            Stat::make('most_popular_product', $popular_products['common_title'] ?? null)
                 ->label(__('shop/product.most_popular_product'))
-                ->description($popular_products['product_title']),
+                ->description($popular_products['product_title'] ?? null),
             Stat::make('Average time on page', $most_expensive_product['common_title'])
                 ->label(__('shop/product.most_expensive_product'))
                 ->description(format_price($most_expensive_product['sell_price']).' ₫'),
