fe557fe944e502521d61b046b0f788974bb61521
tqt97 <kutuanonline199@gmail.com>
2024-08-28 22:09:27 +0700

    edit limit product in page

--- app/Livewire/Product/Index.php
+++ app/Livewire/Product/Index.php
@@ -53,7 +53,7 @@ class Index extends Component
     {
         $products = Product::orderBy('created_at', $this->sort)
             ->whereLike(['product_title', 'common_title'], $this->search)
-            ->paginate(9);
+            ->paginate(30);
 
         return view(
             'livewire.product.index',
