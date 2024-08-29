9e016e65813a302db464f04b9a55e72077a30232
tqt97 <kutuanonline199@gmail.com>
2024-08-29 08:39:08 +0700

    change icon copy

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -7,12 +7,12 @@
         <div class="px-4 py-3 w-full">
             {{-- <span class="text-gray-400 mr-3 uppercase text-xs">Brand</span> --}}
 
-            <p class="text-2xl font-bold text-gray-900 truncate block mt-2 cursor-copy" title="Sao chép"
+            <p class="text-2xl font-bold text-gray-900 truncate block mt-2 cp" title="Sao chép"
                 x-clipboard.raw="{{ $product->common_title }}">
                 {{ $product->common_title }}
             </p>
 
-            <p class="text-lg font-medium text-gray-900 truncate block mt-2 mb-2 cursor-copy" title="Sao chép"
+            <p class="text-lg font-medium text-gray-900 truncate block mt-2 mb-2 cp" title="Sao chép"
                 x-clipboard.raw="{{ $product->product_title }}">{{ $product->product_title }}</p>
             <hr>
             <div class="flex items-center my-2">
@@ -29,7 +29,7 @@
                     {{ $product->qty_per_product }}
                 </span>
             </div>
-            <div class="my-2 cursor-copy" x-clipboard.raw="{{ $product->dosage }}" title="Sao chép">
+            <div class="my-2 cp" x-clipboard.raw="{{ $product->dosage }}" title="Sao chép">
                 <span class="mr-3 text-md text-black mt-2 mb-2 ">
                     <span class="font-semibold text-gray-800">Liều dùng:</span> {{ $product->dosage }}
                 </span>
@@ -38,7 +38,7 @@
                 <span class="mr-3 text-md text-black mt-2 mb-2">
                     <span class="font-semibold text-gray-800">Hạn sử dụng:</span> {{ $product->expiry }}</span>
             </div>
-            <div class="text-black text-justify mt-2 cursor-copy" x-clipboard.raw="{{ $product->description }}">
+            <div class="text-black text-justify mt-2 cp" x-clipboard.raw="{{ $product->description }}">
                 <span class="font-semibold text-gray-800">Mô tả:</span>
                 {{ $product->description }}
             </div>
