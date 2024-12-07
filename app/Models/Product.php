4dfc99f4e87b6ba9f5c1c65a3d699cdf078c686d
tqt97 <kutuanonline199@gmail.com>
2024-12-07 18:00:00 +0700

    fix url

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -70,7 +70,7 @@ class Product extends Model
 
     public function getUrlImage()
     {
-        if(str_contains($this->image, 'https://cdn-images')) {
+        if(str_contains($this->image, 'https://cdn-images') || str_contains($this->image, 'https://cdn2')) {
             return $this->image;
         }
         if ($this->image) {
